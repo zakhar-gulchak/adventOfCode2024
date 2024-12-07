9ca617d68a98ea6722a8e67c10dd92ff9eff0ecb
Zakhar Gulchak <gulchak.zahar@gmail.com>
2024-12-08 00:34:25 +0100

    Add fetchData function for Advent of Code Day 8 inputs
    Created new files part1.js and part2.js to fetch input data asynchronously from the Advent of Code 2024 Day 8 challenge. Implemented error handling to log an error message if data fetching fails.

---
+++ day8/part2.js
@@ -0,0 +1,22 @@
+async function fetchData () {
+  try {
+    const { body } = await fetch('https://adventofcode.com/2024/day/8/input', {
+      headers: {
+        'Cookie': '',
+      }
+    })
+    const reader = await body.getReader()
+    const decoder = new TextDecoder();
+    let data = '';
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) break;
+      data +=  decoder.decode(value, { stream: true });
+    }
+
+  } catch (error) {
+    console.error('Fetching data failed:', error)
+  }
+}
+
+fetchData()
