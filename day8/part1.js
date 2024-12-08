112a0001bc8b96efacb46e46e2621d678129c993
Zakhar Gulchak <gulchak.zahar@gmail.com>
2024-12-08 11:09:54 +0100

    Add antinode calculation for antenna nodes
    This update introduces a mechanism to calculate and store antinodes for each pair of antenna nodes. Additionally, it ensures that valid antinodes are added to a set, and the total count is logged. This enhancement aids in further analysis by providing detailed spatial relationships between nodes.

--- day8/part1.js
+++ day8/part1.js
@@ -14,6 +14,37 @@ async function fetchData () {
       data +=  decoder.decode(value, { stream: true });
     }
 
+    const lines = data.split('\n');
+    lines.pop();
+    const antennas = new Map();
+    // 1. build Map of Nodes
+    for (let i = 0; i < lines.length; i++) {
+      for (let j = 0; j < lines[0].length; j++) {
+        if (lines[i][j] !== '.') {
+          antennas.set(lines[i][j], antennas.has(lines[i][j]) ? [...antennas.get(lines[i][j]), [i, j]] : [[i, j]]);
+        }
+      }
+    }
+    const calcAntinodes = (i1, j1, i2, j2) =>
+      [2 * j1 - j2, 2 * i1 - i2, 2 * j2 - j1, 2 * i2 - i1];
+
+    // 2. for each type calculate antinodes -> add to the set
+    const antinodes = new Set();
+    for (const [_, coordinates] of antennas) {
+      for (let j = 0; j < coordinates.length - 1; j++) {
+        for (let k = j + 1; k < coordinates.length; k++) {
+          const [x1,y1,x2,y2] = calcAntinodes(coordinates[j][0], coordinates[j][1], coordinates[k][0], coordinates[k][1]);
+          if (0 <= x1 && x1 < lines[0].length && 0 <= y1 && y1 < lines.length) {
+            antinodes.add(`[${x1}][${y1}]`);
+          }
+          if (0 <= x2 && x2 < lines[0].length && 0 <= y2 && y2 < lines.length) {
+            antinodes.add(`[${x2}][${y2}]`);
+          }
+        }
+      }
+    }
+
+    console.log(antinodes.size);
   } catch (error) {
     console.error('Fetching data failed:', error)
   }
