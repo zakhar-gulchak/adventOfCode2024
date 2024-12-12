6075eb529f2898097fbcd3c41fcaac86b438077d
Zakhar Gulchak <gulchak.zahar@gmail.com>
2024-12-13 00:53:02 +0100

    day 9 solution

--- day9/part1.js
+++ day9/part1.js
@@ -11,8 +11,39 @@ async function fetchData () {
     while (true) {
       const { done, value } = await reader.read();
       if (done) break;
-      data +=  decoder.decode(value, { stream: true });
+      data += decoder.decode(value, { stream: true });
     }
+
+    // 1. convert to filesystem
+    const filesystem = [];
+    for (let i = 0, len = data.length, fileId = 0; i < len; i++) {
+      if (i % 2 === 0) {
+        for (let j = 0; j < Number(data[i]); j++) {
+          filesystem.push(fileId);
+        }
+        fileId++;
+      } else {
+        for (let j = 0; j < Number(data[i]); j++) {
+          filesystem.push('.');
+        }
+      }
+    }
+    // 2. сompress filesystem
+    for (let i = 0; i < filesystem.length; i++) {
+      while (filesystem.at(-1) === '.') {
+        filesystem.pop();
+      }
+      if (filesystem[i] === '.') {
+        filesystem[i] = filesystem.at(-1);
+        filesystem.pop();
+      }
+    }
+    // 3. calculate checksum
+    let checksum = 0;
+    for (let i = 0; i < filesystem.length; i++) {
+      checksum += i * filesystem[i];
+    }
+    console.log(filesystem.length, checksum);
 
   } catch (error) {
     console.error('Fetching data failed:', error)
--- day9/part2.js
+++ day9/part2.js
@@ -11,10 +11,50 @@ async function fetchData () {
     while (true) {
       const { done, value } = await reader.read();
       if (done) break;
-      data +=  decoder.decode(value, { stream: true });
+      data += decoder.decode(value, { stream: true });
     }
 
+    // 1. convert to filesystem
+    const filesystem = [];
+    for (let i = 0, len = data.length, fileId = 0; i < len; i++) {
+      if (i % 2 === 0) {
+        filesystem.push(Array(Number(data[i])).fill(fileId));
+        fileId++;
+      } else if (data[i] > 0 ) {
+        filesystem.push(Array(Number(data[i])).fill('.'));
+      }
+    }
 
+    // 2. сompress filesystem
+    for (let i = filesystem.length - 1; i > 0; i--) {
+      if (filesystem[i].includes('.')) continue;
+
+      for (let j = 0; j < i; j++) {
+        if (filesystem[j].includes('.') && filesystem[j].length >= filesystem[i].length) {
+          const jLength = filesystem[j].length - filesystem[i].length;
+          filesystem[j] = [...filesystem[i]];
+          filesystem[i] = Array(filesystem[i].length).fill('.');
+          if (jLength > 0) {
+            filesystem.splice(j + 1, 0, Array(jLength).fill('.'));
+            i++;
+          }
+          break;
+        }
+      }
+    }
+
+    // 3. calculate checksum
+    let checksum = 0;
+    let index = 0;
+    for (let i = 0; i < filesystem.length; i++) {
+      for (const block of filesystem[i]) {
+        if (block !== '.') {
+          checksum += index * block;
+        }
+        index++;
+      }
+    }
+    console.log(index, checksum);
   } catch (error) {
     console.error('Fetching data failed:', error)
   }
